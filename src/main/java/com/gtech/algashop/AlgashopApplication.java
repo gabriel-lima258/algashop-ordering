b03268bdee465fe4603848c200d0dc1a0431bd74
Gabriel Lima <gabriel58221@gmail.com>
2026-02-11 13:37:05 -0300

    new project

---
+++ src/main/java/com/gtech/algashop/AlgashopApplication.java
@@ -0,0 +1,13 @@
+package com.gtech.algashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgashopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtech/algashop/AlgashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtech.algashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
